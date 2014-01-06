9fc861032bf23f1c3d243beacaebd5ae6c9b428d
Phillip Seo <pseo@me.com>
2014-01-05 19:50:34 -0500

    Added Player class. Nothing in it

---
+++ Rewind/Player.h
@@ -0,0 +1,13 @@
+//
+//  Player.h
+//  Rewind
+//
+//  Created by PHILLIP SEO on 1/3/14.
+//  Copyright (c) 2014 Phillip Seo. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface Player : SKSpriteNode
+
+@end
---
+++ Rewind/Player.m
@@ -0,0 +1,13 @@
+//
+//  Player.m
+//  Rewind
+//
+//  Created by PHILLIP SEO on 1/3/14.
+//  Copyright (c) 2014 Phillip Seo. All rights reserved.
+//
+
+#import "Player.h"
+
+@implementation Player
+
+@end
